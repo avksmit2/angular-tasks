0266ffe0f2e17880725b838f68f7972714cdeb6e
Angie Smith <avksmit2@gmail.com>
2016-10-16 18:00:13 -0700

    add pies component.

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,11 +1,12 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule }   from '@angular/forms';
+import { PiesListComponent }  from './pies-list.component';
 import { AppComponent }  from './app.component';
 
 @NgModule({
   imports: [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent ],
+  declarations: [ PiesListComponent, AppComponent ],
   bootstrap: [ AppComponent ]
 })
 export class AppModule { }
