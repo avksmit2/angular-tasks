b0206f6d7fc9c97a3832b0e281bdeb205af01e32
Angie Smith <avksmit2@gmail.com>
2016-10-16 18:45:30 -0700

    separate tasks into child components and pass data from parent to child, then actions from child to parent.

--- app/app.module.ts
+++ app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule }   from '@angular/forms';
 import { PiesListComponent }  from './pies-list.component';
 import { AppComponent }  from './app.component';
+import { TaskListComponent }  from './task-list.component';
+import { EditTaskComponent } from './edit-task.component';
 
 @NgModule({
   imports: [ BrowserModule, FormsModule ],
-  declarations: [ PiesListComponent, AppComponent ],
+  declarations: [ PiesListComponent, AppComponent, TaskListComponent, EditTaskComponent ],
   bootstrap: [ AppComponent ]
 })
 export class AppModule { }
